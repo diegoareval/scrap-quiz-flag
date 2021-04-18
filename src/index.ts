f972dc6499ab8c5084c695415e42398f817d3a29
Diego <diego2000avelar@gmail.com>
2021-04-17 19:29:33 -0600

    get scraping url

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
+import { CONTINENTS, getScrappingUrl } from './constants';
+import inquirer from "inquirer"
+
 // program title
 function title(){
     console.log("Extract Data");
     
 }
 // select continents with inquirer
+function selectContinent(){
+   return inquirer.prompt([
+       {
+           type: "list",
+           name: "CONTINENT",
+           message: "What continent do you want to select?",
+           choices: CONTINENTS.map((continent: {name: String}) =>{
+               return continent.name;
+           })
+       }
+   ])
+}
 
 // start scrap
 async function start(){
- title()
+ title();
+ const answers = selectContinent();
+ 
+ const {CONTINENT} = await answers;
+ const continent = CONTINENTS.filter((continent)=> continent.name === CONTINENT)[0].key;
+ const url = getScrappingUrl(continent)
+ console.log(url);
+ 
 }
 
 start();
