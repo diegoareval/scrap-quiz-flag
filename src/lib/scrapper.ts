ed7499686efe4aa44b1234e54838f46095544965
Diego <diego2000avelar@gmail.com>
2021-04-17 20:15:28 -0600

    scrapping all flags

--- src/lib/scrapper.ts
+++ src/lib/scrapper.ts
@@ -1,7 +1,11 @@
 import { getScrappingUrl } from "../constants";
 import $ from "cheerio";
-
+import fs from "fs";
 import requestPromise from "request-promise";
+function extractImage(image: string){
+    const arrayImg = image.split('/');
+    return arrayImg[arrayImg.length -1];
+}
 export function extractData(continent: string) {
   const url = getScrappingUrl(continent);
   const flagItems: Array<object> = [];
@@ -12,12 +16,16 @@ export function extractData(continent: string) {
          const area = $('a', element).attr('data-area');
          const population = $('a', element).attr('data-population');
          const href = $('a', element).attr('href');
-         const name = $('span', element).text()
+         const name = $('span', element).text();
+         const img = $('img', element).attr('src');
+         
          flagItems.push(
              {
-                 area, population, url: href, name
+                 area, population, url: href, name,
+                 img: extractImage(img || "")
              });
      })
+     fs.writeFileSync(`./${continent}.json`, JSON.stringify(flagItems));
      console.log(flagItems);
     
     })
